6cbdc27ae8a453114721b325952022dcd04271ce
ccr5-delta32 <delta32@gmail.com>
2015-07-27 17:55:15 +0200

    First iteration of a simple script to process the 1 .csv file per sample into a single table for further analysis with R

---
+++ convert_csv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+### Bjorn Pieper, End of july 2015
+### This will process the .csv files produced by the MARVIN software containing the measurements
+### of the individual objects. It will produce a single output file for further analysis. 
+
+from os import listdir
+from os.path import isfile, join
+import re
+
+path = "/home/bpuser/server/home/x-perimentz/common_garden/spring2015/Seed_Mass/Field_C_rco/"
+
+## csv files in directory
+files = [ x for x in listdir(path) if re.search('\.csv', x) and isfile(join(path, x)) ]
+
+## Deal with German-style Windows csv to produce a clean list of values
+line1 = True
+for file in files:
+    with open(path + file) as line:
+        header = [ x.strip('\r\n') for x in line.readline().split(';') ]
+        data = [[ re.sub(',', '.', x.strip('\r\n')) for x in y.split(';')] for y in line ] 
+        sample = [ x.strip('.csv') for x in file.split(' ') ]
+        with open('indiv_seeds_processed.tbl', 'ab') as output:
+            if line1 == True:
+                output.write('\t'.join(['sample', 'block', 'column', 'row']) + '\t' + '\t'.join([ x.strip('"') for x in [re.sub('\s', '_', x) for x in header[1:len(header)]] ]) + '\n')
+                line1 = False
+            for out in data:
+                output.write('\t'.join(sample[0:2]) + '\t' + '\t'.join(sample[2].split('.')) + '\t' + '\t'.join(out[1:len(out)]) + '\n')
